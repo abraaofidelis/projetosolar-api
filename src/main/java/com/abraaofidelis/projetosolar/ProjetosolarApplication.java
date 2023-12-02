1100301711322fa97bcde349d67290fef757db8b
abraaofidelis <abraaofidelis@hotmail.com>
2023-12-02 13:42:46 -0300

    Create Spring Boot Project

---
+++ src/main/java/com/abraaofidelis/projetosolar/ProjetosolarApplication.java
@@ -0,0 +1,13 @@
+package com.abraaofidelis.projetosolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abraaofidelis/projetosolar/ProjetosolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraaofidelis.projetosolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
